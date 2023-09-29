9e38abfa65e2c4035d1aefbcd7a413f4ff7c36d6
Daniel Riley <dann.riley@icloud.com>
2023-09-29 16:10:32 +0200

    create header with navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import GlobalStyle from "../styles";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
+      <header>
+        <h1>Zeitgenössisch</h1>
+        <nav>
+          <Link href="/spotlight">Spotlight</Link>
+          <Link href="/">Art Pieces</Link>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </>
   );
